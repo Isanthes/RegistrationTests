c9ac65143d6059c6c37faf83a6097171fdce308e
Cristian Gazdac <cristi.gazdac@gmail.com>
2022-02-19 22:53:35 +0200

    TestData

---
+++ DataModels/Registration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace RegistrationTests.DataModels
+{
+    [XmlRoot(ElementName = "registrationData")]
+    public class Registration
+    {
+		[XmlElement(ElementName = "user")]
+		public string User { get; set; }
+
+		[XmlElement(ElementName = "pass")]
+		public string Pass { get; set; }
+
+		[XmlElement(ElementName = "confirmPass")]
+		public string ConfirmPass { get; set; }
+
+		[XmlElement(ElementName = "title")]
+		public string Title { get; set; }
+
+		[XmlElement(ElementName = "firstName")]
+		public string FirstName { get; set; }
+
+		[XmlElement(ElementName = "lastName")]
+		public string LastName { get; set; }
+
+		[XmlElement(ElementName = "email")]
+		public string Email { get; set; }
+
+		[XmlElement(ElementName = "nationality")]
+		public string Nationality { get; set; }
+	}
+}
--- Tests/SiteRegistration/FirstRegistrationTests.cs
+++ Tests/SiteRegistration/FirstRegistrationTests.cs
@@ -6,7 +6,9 @@ using RegistrationTests.POM;
 using RegistrationTests.Utilities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace RegistrationTests.Tests
 {
@@ -14,9 +16,65 @@ namespace RegistrationTests.Tests
     {
         
         string url = Utils.GetUrl();
-   
-        [Test]
-        public void RegistrationHappyFlow()
+
+        private static IEnumerable<TestCaseData> GetRegistationData()
+        {
+            yield return new TestCaseData("Hitsugayat", "Conquer.1234", "Conquer.1234", "Mr.","Andrei","Greuceanu","test@btrl.ro","American");
+            yield return new TestCaseData("Atena", "Olimp.1234", "Olimp.1234", "Ms.", "Andreea", "Greuceanu", "test@btrl.ro", "American");
+            yield return new TestCaseData("Aphrodita", "Roma.2345", "Roma.2345", "Ms.", "Bianca", "Andreescu", "test@btrl.ro", "Canadian");
+            yield return new TestCaseData("Apolo", "Conquer.1234", "Conquer.1234", "Mr.", "Adrian", "Elcian", "test@btrl.ro", "Romanian");
+        }
+        private static IEnumerable<TestCaseData> GetRegistationDataCsv()
+        {
+            foreach (var values in Utils.GetGenericData("TestData\\registrationData.csv"))
+            {
+                yield return new TestCaseData(values);
+            }
+        }
+        private static IEnumerable<TestCaseData> GetOnlyRequiredDataCsv()
+        {
+            foreach (var values in Utils.GetGenericData("TestData\\onlyRequiredData.csv"))
+            {
+                yield return new TestCaseData(values);
+            }
+        }
+        private static IEnumerable<TestCaseData> GetRegistationDataCsv1()
+        {
+            var csvData = Utils.GetDataTableFromCsv("TestData\\registrationData.csv");
+            for (int i = 0; i < csvData.Rows.Count; i++)
+            {
+                yield return new TestCaseData(csvData.Rows[i].ItemArray);
+            }
+        }
+        private static IEnumerable<TestCaseData> GetRegistationDataExcel()
+        {
+            var excelData = Utils.GetDataTableFromExcel("TestData\\RegistrationData.xlsx");
+            for (int i = 1; i < excelData.Rows.Count; i++)
+            {
+                yield return new TestCaseData(excelData.Rows[i].ItemArray);
+            }
+        }
+        private static IEnumerable<TestCaseData> GetRegistationDataJson()
+        {
+            var regData = Utils.JsonRead<DataModels.Registration>("TestData\\registrationData.json");
+            yield return new TestCaseData(regData.User, regData.Pass, regData.ConfirmPass, regData.Title, regData.FirstName, regData.LastName, regData.Email, regData.Nationality);
+        }
+        private static IEnumerable<TestCaseData> GetRegistationDataXml()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(DataModels.Registration));
+            foreach (var file in Utils.GetAllFilesInFolderExt("TestData\\", "*.xml"))
+            {
+                Console.WriteLine("Testing with file: " + file);
+                using (Stream reader = new FileStream(file, FileMode.Open))
+                {
+                    var regData = (DataModels.Registration)serializer.Deserialize(reader);
+                    yield return new TestCaseData(regData.User, regData.Pass, regData.ConfirmPass, regData.Title, regData.FirstName, regData.LastName, regData.Email, regData.Nationality);
+                }
+            }
+        }
+
+        [Test, TestCaseSource("GetRegistationDataXml")]
+        public void RegistrationHappyFlow(string user, string pass, string confirmPass, string title, string firstName, string lastName, string email, string nationality)
         {
             driver.Navigate().GoToUrl(url + "registration");
             RegistrationPage happyFlow = new RegistrationPage(driver);
@@ -31,7 +89,7 @@ namespace RegistrationTests.Tests
             Assert.AreEqual("Date of birth", happyFlow.getLabel(happyFlow.getDateOfBirthLabel()));
             Assert.AreEqual("Nationality", happyFlow.getLabel(happyFlow.getNationalityLabel()));
             Assert.AreEqual("4 to 35 letters, numbers or underscore.", happyFlow.getDescription(happyFlow.getUsernameDescription()));
-            happyFlow.FirstRegistration("Isanthes","password123","password123","Mr.","Verde","Crud","verdecrud@gmail.com","American");
+            happyFlow.FirstRegistration(user,pass,confirmPass,title,firstName,lastName,email,nationality);
            
         }
 
@@ -61,13 +119,13 @@ namespace RegistrationTests.Tests
             Assert.AreEqual("You need to accept our T&C!", errorMessage.getError(errorMessage.getTermsError()));
         }
 
-        [Test]
-        public void OnlyRequired()
+        [Test, TestCaseSource("GetOnlyRequiredDataCsv")]
+        public void OnlyRequired(string user, string pass, string confirmPass, string email)
         {
             driver.Navigate().GoToUrl(url + "registration");
             RegistrationPage or = new RegistrationPage(driver);
             Assert.AreEqual("Registration", or.ChackPage());
-            or.RegistrationOnlyRecuiredFields("Hitsugayat", "123asdfg", "123assdf", "test@gmail.com");
+            or.RegistrationOnlyRecuiredFields(user,pass,confirmPass,email);
         }
         
         [Test]
